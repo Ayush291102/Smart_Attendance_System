c2f9a563c63c087219652ffa5bb8d78e9603f847
Ayush291102 <154556786+Ayush291102@users.noreply.github.com>
2023-12-26 19:26:31 +0530

    Add files via upload

---
+++ FaceRecognition.py
@@ -0,0 +1,115 @@
+import face_recognition
+import cv2
+import numpy as np
+import csv
+import os
+from datetime import datetime
+
+video_capture = cv2.VideoCapture(0)
+
+ab_image = face_recognition.load_image_file(r"photos\ab.jpeg")
+ab_encoding = face_recognition.face_encodings(ab_image)[0]
+
+sk_image = face_recognition.load_image_file(r"photos\sk.jpeg")
+sk_encoding = face_recognition.face_encodings(sk_image)[0]
+
+srk_image = face_recognition.load_image_file(r"photos\srk.jpeg")
+srk_encoding = face_recognition.face_encodings(srk_image)[0]
+
+ak_image = face_recognition.load_image_file(r"photos\ak.jpg")
+ak_encoding = face_recognition.face_encodings(ak_image)[0]
+
+rk_image = face_recognition.load_image_file(r"photos\rk.jpg")
+rk_encoding = face_recognition.face_encodings(rk_image)[0]
+
+akki_image = face_recognition.load_image_file(r"photos\akki.jpg")
+akki_encoding = face_recognition.face_encodings(akki_image)[0]
+
+rvs_image = face_recognition.load_image_file(r"photos\rvs.jpg")
+rvs_encoding = face_recognition.face_encodings(rvs_image)[0]
+
+as_image = face_recognition.load_image_file(r"photos\as.png")
+as_encoding = face_recognition.face_encodings(as_image)[0]
+
+
+known_face_encoding = [
+ab_encoding,
+sk_encoding,
+srk_encoding,
+ak_encoding,
+rk_encoding,
+akki_encoding,
+rvs_encoding,
+as_encoding
+]
+
+known_faces_names = [
+"Amitabh",
+"Salman",
+"ShahRukh",
+"Akshay",
+"Ranbir",
+"Akshay",
+"Ranveer",
+"Ayush"
+]
+
+students = known_faces_names.copy()
+
+face_locations = []
+face_encodings = []
+face_names = []
+s=True
+
+now = datetime.now()
+current_date = now.strftime("%Y-%m-%d")
+
+f = open(current_date+'.csv','w+',newline = '')
+lnwriter = csv.writer(f)
+
+while True:
+    _,frame = video_capture.read()
+    small_frame = cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
+    rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+    if s:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame,face_locations)
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encoding,face_encoding)
+            name=""
+            face_distance = face_recognition.face_distance(known_face_encoding,face_encoding)
+            best_match_index = np.argmin(face_distance)
+            if matches[best_match_index]:
+                name = known_faces_names[best_match_index]
+ 
+            face_names.append(name)
+            if name in known_faces_names:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                bottomLeftCornerOfText = (10,100)
+                fontScale              = 1.5
+                fontColor              = (255,0,0)
+                thickness              = 3
+                lineType               = 2
+ 
+                cv2.putText(frame,name+' Present', 
+                    bottomLeftCornerOfText, 
+                    font, 
+                    fontScale,
+                    fontColor,
+                    thickness,
+                    lineType)
+ 
+                if name in students:
+                    students.remove(name)
+                    print(students)
+                    current_time = now.strftime("%H-%M-%S")
+                    lnwriter.writerow([name,current_time])
+    cv2.imshow("attendence system",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+ 
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Smart_Attendance_System
-Advanced Attendance System created using openCV
+Welcome to the Smart Attendance System Project craeted using openCV
+
+First of all ypu need to import the following packages to your system-
+1. openCV
+2. numpy
+3. os
+4. csv
+5. face_recognition
+6. datetime
+
+If the pic of the student is shown to the camera, there name gets removed from the absentees list
